21e31dd7edb9cc4bb1ad8e300c19d8dd64abb08c
Fabian <fabram613@gmail.com>
2020-05-24 17:20:38 -0500

    add start function and user prompt

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,8 +1,9 @@
-//Including the required npm packages
+//REQUIRE
+//=======
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
-// Creating the connection information for the sql database
+// Creating the connection for the sql database
 var connection = mysql.createConnection({
 	host: "localhost",
 	port: 3306,
@@ -14,7 +15,36 @@ var connection = mysql.createConnection({
 // Connecting to the mysql server and sql database
 connection.connect(function(err) {
 	if (err) throw err;
-	console.log("\n==== WELCOME TO BAMAZON CUSTOMER VIEW ====\n\nYou are now connected as id " + connection.threadId + "\n");
+	console.log("\n==== WELCOME TO BAMAZON! ====\n\nYou are now connected as id " + connection.threadId + "\n");
 	// Run the start function after the connection is made to begin the application
 	start();
 });
+
+// Function that prompts the user
+function start() {
+	//Displaying all of the items available for sale.
+	console.log("These are all the products available on BAMAZON at the moment: \n");
+	connection.query("SELECT id, product_name, price FROM products", function(error, results) {
+		if (error) throw error;
+
+		// Log all results of the SELECT statement
+		console.log(JSON.stringify(results, null, " ") + "\n\n=========================================================\n\n");
+
+		// Prompt users with two messages:
+		inquirer
+			.prompt([
+				{
+					//Asking for ID of the product they would like to buy.
+					name: "idChosen",
+					type: "input",
+					message: "Please enter the ID number of the product you wish to purchase: "
+				},
+				{
+					//Asking how many units of the product they would like to buy.
+					name: "unitsPurchased",
+					type: "input",
+					message: "How many units of this product would you like to purchase?"
+				}
+		]).
+		then(function(answer) {
+			console.log("\nYOUR ORDER HAS BEEN PLACED!!!\n\nLet me check our stock for you...\n");
